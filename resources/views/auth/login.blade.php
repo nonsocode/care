ca407e79e5a62bcd994d4a8b2bf37afa8cee7c67
Nonso <nonso4yoo@gmail.com>
2017-08-13 09:54:16 +0100

    Add Reset Password form and logic

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,23 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    <form class="login-form" action="{{route('login')}}" method="POST">
+      @if (session()->has('error'))
+        <div class="alert error">
+        {{session('error')}}
+        <span class="close">&times;</span>
         </div>
-    </div>
-</div>
+      @endif
+      @if ($errors->count())
+        <div class="alert error">
+        {{$errors->first()}}
+        <span class="close">&times;</span>
+        </div>
+      @endif
+      {{csrf_field()}}
+      <input type="email" class="{{$errors->has('email') ? "has-error" : "" }}" name="email" placeholder="Email" value="{{old('email')}}" autofocus>
+      <input type="password" name="password" placeholder="password"/>
+      <button>login</button>
+      <p class="message">Forgot Password? <a href="{{ route('password.request') }}">Click Here</a></p>
+    </form>
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,60 +1,28 @@
 <!DOCTYPE html>
 <html >
-<head>
-  <meta charset="UTF-8">
-  <title>Care | Admin</title>
-  
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
-  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-
-  <link rel="stylesheet" href="{{asset('css/auth.css')}}">
-
-  
-</head>
-
-<body>
-
-  <div class="container">
-    <div class="info">
-      <h1>Care.com.ng</h1>
-    </div>
-  </div>
-  <div class="form">
-  <div class="thumbnail"><img src="{{ asset('img/default-avatar.png') }}"/></div>
-    <form class="register-form">
-      <input type="text" placeholder="name" autofocus />
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p class="message">Forgot Password? <a href="#">Click here</a></p>
-    </form>
-    <form class="login-form" action="{{route('login')}}" method="POST">
-      @if (session()->has('error'))
-        <div class="alert error">
-        {{session('error')}}
-        <span class="close">&times;</span>
+    <head>
+        <meta charset="UTF-8">
+        <title>Care | Admin</title>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+        <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
+        <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+        <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+        <link rel="stylesheet" href="{{asset('css/auth.css')}}">
+        
+    </head>
+    <body>
+        <div class="container">
+            <div class="info">
+                <h1>Care.com.ng</h1>
+            </div>
         </div>
-      @endif
-      @if ($errors->count())
-        <div class="alert error">
-        {{$errors->first()}}
-        <span class="close">&times;</span>
+        <div class="form">
+            <div class="thumbnail"><img src="{{ asset('img/default-avatar.png') }}"/></div>
+            @yield('content');
         </div>
-      @endif
-      {{csrf_field()}}
-      <input type="email" class="{{$errors->has('email') ? "has-error" : "" }}" name="email" placeholder="Email" value="{{old('email')}}" autofocus>
-      <input type="password" name="password" placeholder="password"/>
-      <button>login</button>
-      <p class="message">Forgot Password? <a href="{{ route('password.request') }}">Click Here</a></p>
-    </form>
-  </div>
-  <script src='{{ asset('js/jquery.js') }}'></script>
-
-  <script src="{{asset('js/auth.js')}}"></script>
-
-</body>
+        <script src='{{ asset('js/jquery.js') }}'></script>
+        <script src="{{asset('js/auth.js')}}"></script>
+    </body>
 </html>
